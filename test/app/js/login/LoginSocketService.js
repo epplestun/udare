f00c0f963d7d4c7faa01a88946eb015a1af0628d
epplestun <epplestun@epplestun.(none)>
2013-12-26 21:49:51 +0100

    Socket example

---
+++ test/app/js/login/LoginSocketService.js
@@ -0,0 +1,11 @@
+define([
+  'socketio'
+  ], function(io) {
+  
+  var socket = io.connect('http://localhost:3000');
+  socket.on('news', function (data) {
+    console.log(data);
+    //socket.emit('my other event', { my: 'data' });
+  });
+  
+});
